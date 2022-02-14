dbcd6f25dd5bad9c33b3c0805ef0b5f357fb5245
Hugo Hache <hugo.hache@spaceable.org>
2022-02-14 13:22:27 +0100

    Panda server on port 8080

---
+++ lib/panda/application.ex
@@ -0,0 +1,22 @@
+defmodule Panda.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+  require Logger
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: Panda.Router, options: [port: 8080]}
+    ]
+
+    Logger.info("Starting Panda server on port 8080 ...")
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Panda.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ lib/panda/router.ex
@@ -0,0 +1,14 @@
+defmodule Panda.Router do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  get "/" do
+    send_resp(conn, 200, "Welcome 🐼")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Oops!")
+  end
+end
