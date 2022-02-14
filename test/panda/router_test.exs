b11892a263083663dcd070be8b2e2d7f00cf0766
Hugo Hache <hugo.hache@spaceable.org>
2022-02-14 13:30:23 +0100

    Test router

---
+++ test/panda/router_test.exs
@@ -0,0 +1,28 @@
+defmodule Example.RouterTest do
+  use ExUnit.Case
+  use Plug.Test
+
+  alias Panda.Router
+
+  @opts Router.init([])
+
+  test "returns welcome" do
+    conn =
+      conn(:get, "/")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Welcome 🐼"
+  end
+
+  test "returns 404" do
+    conn =
+      conn(:get, "/missing")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 404
+    assert conn.resp_body == "Oops!"
+  end
+end
