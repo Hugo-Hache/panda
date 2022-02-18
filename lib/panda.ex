5e069ca3b27a5634d69208979fbde46d05f10abe
Hugo Hache <hugo.hache@spaceable.org>
2022-02-18 16:11:29 +0100

    Remove partial doc

--- lib/panda.ex
+++ lib/panda.ex
@@ -1,28 +1,6 @@
 defmodule Panda do
   require Logger
 
-  @moduledoc """
-  Documentation for `Panda`.
-  """
-
-  @doc """
-  Fetch next five upcoming matches, ordered by ascending begin_at.
-
-  ## Examples
-
-      iex> Panda.upcoming_matches()
-      [
-        %Panda.Match{
-          begin_at: "2022-02-14T15:00:00Z",
-          id: 621480,
-          name: "Lower Bracket Round 3 Match 1: HR vs V-Gaming",
-          scheduled_at: "2022-02-14T15:00:00Z",
-          draw: false,
-          winner_id: nil
-          opponents: [...]
-        }
-      ]
-  """
   @spec upcoming_matches :: [Panda.Match.t()]
   def upcoming_matches do
     Panda.API.Match.upcoming()
